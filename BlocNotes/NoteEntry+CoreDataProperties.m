5d41ca53739e2685ac3e79ec9e3758630ffb7764
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-06-19 16:51:20 -0400

    Add CoreDataStack file and NoteEntry ManagedObject subclass

---
+++ BlocNotes/NoteEntry+CoreDataProperties.m
@@ -0,0 +1,19 @@
+//
+//  NoteEntry+CoreDataProperties.m
+//  BlocNotes
+//
+//  Created by Joanna Lingenfelter on 6/19/16.
+//  Copyright © 2016 JoLingenfelter. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "NoteEntry+CoreDataProperties.h"
+
+@implementation NoteEntry (CoreDataProperties)
+
+@dynamic body;
+@dynamic date;
+
+@end
