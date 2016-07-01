0923aaa079140b656cd27894c9f5f90885da2b37
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-07-01 15:42:11 -0400

    Added Share Extension

---
+++ BlocNotes/AddToBlocNotes/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  AddToBlocNotes
+//
+//  Created by Joanna Lingenfelter on 7/1/16.
+//  Copyright © 2016 JoLingenfelter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
