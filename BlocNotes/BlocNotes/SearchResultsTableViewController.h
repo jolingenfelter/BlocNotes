ff3bf560cd9bdeef969e7330df477378525f6e0b
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-07-07 16:56:51 -0400

    AddedSearchResultsViewController

---
+++ BlocNotes/BlocNotes/SearchResultsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchResultsTableViewController.h
+//  BlocNotes
+//
+//  Created by Joanna Lingenfelter on 7/7/16.
+//  Copyright © 2016 JoLingenfelter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchResultsTableViewController : UITableViewController
+
+@end
---
+++ BlocNotes/BlocNotes/SearchResultsTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  SearchResultsTableViewController.m
+//  BlocNotes
+//
+//  Created by Joanna Lingenfelter on 7/7/16.
+//  Copyright © 2016 JoLingenfelter. All rights reserved.
+//
+
+#import "SearchResultsTableViewController.h"
+
+@implementation SearchResultsTableViewController
+
+@end
